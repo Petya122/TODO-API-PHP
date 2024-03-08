60e3de03f8a0fd82c8dd21367b46892de88f7fdb
Petya122 <wildpeter10@gmail.com>
2024-03-08 12:18:29 +0100

    Add basic files

---
+++ public/app.php
@@ -0,0 +1,7 @@
+<?php
+
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
